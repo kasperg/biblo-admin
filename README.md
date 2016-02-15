66c2aa6cdfe8eb9487913452843c8e58d55c05bf
Kasper Garnaes <kasper.garnaes@gmail.com>
2016-02-15 10:54:12 +0100

    Initial version of docker development environment
    Use a locally build web image to support Drush and drupal-composer
    project structure.
    
    Provide default services.yml and trimmed down settings.php using Docker.
    
    Update scripts. We do not want drupal-project to create settings or 
    services files during composer install.
    
    Provide minimal database. This is primarily for setting a shared
    system.site uuid configuration across environments.
    
    Initial version of README.

---
+++ README.md
@@ -0,0 +1,18 @@
+# biblo.dk Administration
+
+This is the administration interface for biblo.dk based on Drupal 8.
+
+The project structure is based on [`drupal-composer/drupal-project`](https://github.com/drupal-composer/drupal-project).
+
+## Development dependencies
+
+* Docker and Docker Compose
+* Composer
+
+## Installation instructions
+
+1. Clone this repository
+2. From the root of the cloned repository:
+	1. Run `docker-compose build`
+	2. Run `docker-compose up`
+3. Profit!
---
+++ docker/docker.settings.php
@@ -0,0 +1,20 @@
+<?php
+
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => 'db',
+  'username' => 'db',
+  'password' => 'bP4pFxUvFcF2b1bV',
+  'host' => 'db',
+  'prefix' => '',
+);
+
+$config_directories = array(
+  CONFIG_SYNC_DIRECTORY => '/var/www/web/config/sync',
+);
+
+$settings['hash_salt'] = '3yR1W3__wdXH8R_08gA1ZtNeuMOlFfwffJIk2vvXX-Yg6B4O3Q0OMcpVuP3_Y6go3m7Cy7k3pw';
+$settings['update_free_access'] = FALSE;
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+$settings['install_profile'] = 'minimal';
