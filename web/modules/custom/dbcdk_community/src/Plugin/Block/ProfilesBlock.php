fff45e89f1254c0372e92c25582ed3cf95d0f2d6
Kasper Garnaes <kasper.garnaes@gmail.com>
2016-04-07 13:30:31 +0200

    Determine quarantined profiles from specific time
    Using within the method makes mocking expected arguments difficult.

--- web/modules/custom/dbcdk_community/src/Plugin/Block/ProfilesBlock.php
+++ web/modules/custom/dbcdk_community/src/Plugin/Block/ProfilesBlock.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\dbcdk_community\Plugin\Block;
 
+use DateTime;
 use DBCDK\CommunityServices\ApiException;
 use DBCDK\CommunityServices\Model\Profile;
 use Psr\Log\LoggerAwareTrait;
@@ -192,8 +193,9 @@ class ProfilesBlock extends BlockBase implements ContainerFactoryPluginInterface
 
       // Fetch a list or profiles with an active quarantine.
       if ($this->filterQuery['quarantined']) {
-        $profiles = $this->profileRepository->getQuarantinedProfiles($page_filter);
-        $profile_count = $this->profileRepository->countQuarantinedProfiles($filter);
+        $now = new DateTime(date('c', REQUEST_TIME));
+        $profiles = $this->profileRepository->getQuarantinedProfiles($now, $page_filter);
+        $profile_count = $this->profileRepository->countQuarantinedProfiles($now, $filter);
       }
       // Fetch a list of profiles.
       else {
